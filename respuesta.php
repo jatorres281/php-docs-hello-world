5aa445f807bbee40199f601951135641668d4b20
jatorres281 <80855448+jatorres281@users.noreply.github.com>
2025-06-20 18:15:41 +0200

    Add files via upload

--- respuesta.php
+++ respuesta.php
@@ -34,7 +34,7 @@ try {
     // Ejemplo: consulta sencilla
     $stmt = $pdo->query('SELECT * FROM prueba;');
     $fila = $stmt->fetch();
-    echo "Conectado correctamente. Hora del servidor: " . $fila['Id'];
+    echo "Conectado correctamenteeee. Hora del servidor: " . $fila[1];
 } catch (PDOException $e) {
     error_log('Error de conexión PDO: ' . $e->getMessage());
     echo "Error al conectar con la base de datos: " . htmlspecialchars($e->getMessage());
