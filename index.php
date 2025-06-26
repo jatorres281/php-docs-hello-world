52d04360b0c0e895e70b79d5e6667af7cca3cea2
jatorres281 <80855448+jatorres281@users.noreply.github.com>
2025-06-26 21:03:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<FONT COLOR="RED"><FONT SIZE="20"><b>Pagina de acceso a los clientes de un agente</b></FONT></FONT>
+<p>
+<FORM action="respuesta.php" method="POST">
+Usuario:<INPUT type="text" name="usuario"><p>
+Contrase&ntilde;a:<INPUT type="text" name="contrasena"><p>
+<INPUT type="submit" name="enviar">
+
+</FORM>
