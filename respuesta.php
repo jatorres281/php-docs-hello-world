fc5f87d6b1f323b0eec68270b5f6aff69566356c
jatorres281 <80855448+jatorres281@users.noreply.github.com>
2025-06-30 19:18:09 +0200

    Add files via upload

--- respuesta.php
+++ respuesta.php
@@ -2,43 +2,48 @@
 
 <?php
 
-//Conectamos con la base de datos
-$con=mysqli_connect("mysql-cefire01-juan.mysql.database.azure.com","cefire","Alex1503","empresa");
+$host = getenv('DB_HOST');
+$dbname = "empresa";
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
+	//$host = 'mysql-cefire01-juan.mysql.database.azure.com';
+	//$dbname = 'empresa';
+	//$user = 'cefire';
+	//$password = 'Alex1503';
 
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
+	try {
 
-//Lanzamos una consulta para ver si existe alg√∫n agente con el login y passowrd escritos en el formulario
-$sql="select * from agentes where login='".$_POST['usuario']."' and password='".$_POST['contrasena']."'";
-$resul=mysqli_query($con,$sql);
+		//Conectamos con la base de datos
+		$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
 
-//Si no ha devuelto un resultado
-if (mysqli_num_rows($resul)==0)    
-	//Datos incorrectos
-   	echo "<font size='14' color='red'>Nombre de usuario o contrase√±a erroneos</font><p>\n\n";    
-//S√≠ ha devuelto un resultado
-else{
-	//recupero el resultado y escribo el nombre y el apellido del agente
-	$arr_resul= mysqli_fetch_array($resul,MYSQLI_NUM);
-	echo "Bienvenido <u><b>".$arr_resul[1]." ".$arr_resul[2]."</u></b>. Estos son tus clientes:<p>\n\n";
-     	// recupero el NIF del agente y construyo la siguiente consulta
-	$NIF=$arr_resul[0];
-	$sql="select * from clientes where NIFAgente='$NIF'";
-     	//Lanzo la consulta para ver si existe alg√∫n cliente para ese agente
-	$resul=mysqli_query($con,$sql);
-     	echo "<table border=1>\n<tr><td><b>NIF</b></td><td><b>Nombre</b></td><td><b>Apellidos</b></td><td><b>Telefono</b></td><td><b>Saldo</b></td></tr>\n";
-     	//recorro la respuesta fila por fila, escribiendo en una tabla cada cliente de ese agente
-	while ($arr_resul=mysqli_fetch_array($resul,MYSQLI_NUM))
-		echo "<tr><td>".$arr_resul[0]."</td><td>".$arr_resul[1]."</td><td>".$arr_resul[2]."</td><td>".$arr_resul[3]."</td><td>".$arr_resul[4]."</td></tr>\n";
-		echo "</table><p>\n\n";
-     }
-         
-// Liberamos espacio ocupado con el resultado de la consulta
-mysqli_free_result($resul);
-// Cerramos la conexi√≥n con el servidor de BD
-mysqli_close($con);
+		//Lanzamos una consulta para ver si existe alg˙n agente con el login y passowrd escritos en el formulario	
+		$sql="select * from agentes where login='".$_POST['usuario']."' and password='".$_POST['contrasena']."'";
+		$resul = $pdo->query($sql);
+ 
+		//Si no ha devuelto un resultado
+		if (!$agente = $resul->fetch())    
+			//Datos incorrectos
+   			echo "<font size='14' color='red'>Nombre de usuario o contrase&ntilde;a erroneos</font><p>\n\n";    
+		//SÌ ha devuelto un resultado
+		else{
+			//recupero el resultado y escribo el nombre y el apellido del agente
+ 			echo "Bienvenido <u><b>{$agente["Nombre"]} {$agente["Apellidos"]}</u></b>. Estos son tus clientes:<p>\n\n";
+     			// recupero el NIF del agente y construyo la siguiente consulta
+			$NIF=$agente["NIF"];
+			$sql="select * from clientes where NIFAgente='$NIF'";
+     			//Lanzo la consulta para ver si existe alg˙n cliente para ese agente
+			$resul=$pdo->query($sql);
+     			echo "<table border=1>\n<tr><td><b>NIF</b></td><td><b>Nombre</b></td><td><b>Apellidos</b></td><td><b>Telefono</b></td><td><b>Saldo</b></td></tr>\n";
+     			//recorro la respuesta fila por fila, escribiendo en una tabla cada cliente de ese agente
+			while ($cliente=$resul->fetch()) {
+				echo "<tr><td>".$cliente[0]."</td><td>".$cliente[1]."</td><td>".$cliente[2]."</td><td>".$cliente[3]."</td><td>".$cliente[4]."</td></tr>\n"; }
+			echo "</table><p>\n\n";
+		}
+    
+	}   catch (PDOException $e) {
+    	// Manejo de errores
+    	echo "Error en la conexiÛn o consulta: " . $e->getMessage();
+	}
 ?>
-<a href="index.html">Volver a la p√°gina anterior</a>
+<a href="index.html">Volver a la p&aacute;gina anterior</a>
