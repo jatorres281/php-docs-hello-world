a171bdf4bd30f31058b9eb86cfb5c504b67fc727
jatorres281 <80855448+jatorres281@users.noreply.github.com>
2025-06-30 09:08:15 +0200

    Add files via upload

--- respuesta.php
+++ respuesta.php
@@ -1,27 +1,19 @@
-<font size="20"><b>CLIENTESss</b></font><p>
+<font size="20"><b>CLIENTES</b></font><p>
 
 <?php
 
-$dbHost = getenv('DB_HOST');
-$dbName = "empresa";
-$dbUser = getenv('DB_USER');
-$dbPass = getenv('DB_PASSWORD');
-echo getenv('DB_HOST')."   ".getenv('DB_USER')."   ".getenv('DB_PASSWORD');
-
-
-
 //Conectamos con la base de datos
-$con=mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
+$con=mysqli_connect("localhost","root","","empresa");
 
-//Lanzamos una consulta para ver si existe algÃºn agente con el login y passowrd escritos en el formulario
+//Lanzamos una consulta para ver si existe algún agente con el login y passowrd escritos en el formulario
 $sql="select * from agentes where login='".$_POST['usuario']."' and password='".$_POST['contrasena']."'";
 $resul=mysqli_query($con,$sql);
 
 //Si no ha devuelto un resultado
 if (mysqli_num_rows($resul)==0)    
 	//Datos incorrectos
-   	echo "<font size='14' color='red'>Nombre de usuario o contraseÃ±a erroneos</font><p>\n\n";    
-//SÃ­ ha devuelto un resultado
+   	echo "<font size='14' color='red'>Nombre de usuario o contraseña erroneos</font><p>\n\n";    
+//Sí ha devuelto un resultado
 else{
 	//recupero el resultado y escribo el nombre y el apellido del agente
 	$arr_resul= mysqli_fetch_array($resul,MYSQLI_NUM);
@@ -29,7 +21,7 @@ else{
      	// recupero el NIF del agente y construyo la siguiente consulta
 	$NIF=$arr_resul[0];
 	$sql="select * from clientes where NIFAgente='$NIF'";
-     	//Lanzo la consulta para ver si existe algÃºn cliente para ese agente
+     	//Lanzo la consulta para ver si existe algún cliente para ese agente
 	$resul=mysqli_query($con,$sql);
      	echo "<table border=1>\n<tr><td><b>NIF</b></td><td><b>Nombre</b></td><td><b>Apellidos</b></td><td><b>Telefono</b></td><td><b>Saldo</b></td></tr>\n";
      	//recorro la respuesta fila por fila, escribiendo en una tabla cada cliente de ese agente
@@ -40,7 +32,7 @@ else{
          
 // Liberamos espacio ocupado con el resultado de la consulta
 mysqli_free_result($resul);
-// Cerramos la conexiÃ³n con el servidor de BD
+// Cerramos la conexión con el servidor de BD
 mysqli_close($con);
 ?>
-<a href="index.html">Volver a la pÃ¡gina anterior</a>
+<a href="index.html">Volver a la página anterior</a>
